f65fc82cb1eb8b8f5e0e9225d9b9d6bbd8b45229
Suan Lian <suanl0002@gmail.com>
2023-03-25 01:04:45 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();    
+}
+
+function appendCat(name) {
+   const newCat = [...cats, "Broom"];
+   return newCat;
+}
+
+function prependCat(name) {
+    const newCat_2 = ["Arnold", ...cats];
+    return newCat_2;
+}
+
+function removeLastCat() {
+    const remCat_1 = cats.slice(0, cats.length -1);
+    return remCat_1;
+}
+
+function removeFirstCat() {
+    const remCat_2 = cats.slice(1);
+    return remCat_2;
+}
